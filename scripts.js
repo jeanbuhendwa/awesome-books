c5abd420d33745a4c1f9c4100a2b4797d9f577f0
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-02-13 14:16:13 +0300

    Add function to Js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,46 @@
 const books_list = document.querySelector(".books");
+
+const bookName = document.getElementById("book_name");
+const autName = document.getElementById("auther_name");
+const addBtn = document.getElementById("addButton");
+
+// const bookSection = document.getElementById("book_section");
+
+let bookStore = [
+  {
+    title: "Book1",
+    author: "Authors1",
+  },
+  {
+    title: "Book2",
+    author: "Authors2",
+  },
+];
+// Add book function
+function displayBook(book) {
+    books_list.innerHTML += `
+            <li>
+                <span class="name">${book.title}</span>
+                <span class="auther">${book.author}</span>
+                <button type="button" class="delete">Remove</button>
+                <hr>
+            </li>
+        `;
+  }
+
+for (let i = 0; i < bookStore.length; i++) {
+    displayBook(bookStore[i]);
+}
+
+
+function addBook() {
+  let objBook = {
+    title : bookName.value,
+    author : autName.value
+  }
+
+  bookStore.push(objBook);
+  displayBook(objBook);
+}
+
+addBtn.addEventListener("click", addBook);
